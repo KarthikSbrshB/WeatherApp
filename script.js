9f69ae0d8ec588f1d0ebd045f8f50fa1cb7a3f40
KarthikSbrshB <karthiksb.programming@gmail.com>
2023-11-05 23:30:28 +0530

    final working script

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ search.addEventListener('click', () => {
     if (city === '')
         return;
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4418a91482ac6355afae1999f7aeffba`).then(response => response.json()).then(json => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=4418a91482ac6355afae1999f7aeffba`).then(response => response.json()).then(json => {
         
         if(json.cod === '404'){
 
